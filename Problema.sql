4281b2f601978fe9a5881813a1cf6de839a78dc7
GabistorM <gabrielpaesdeandrade1991@gmail.com>
2024-01-17 21:55:54 -0300

    Create Problema.sql

---
+++ Problema.sql
@@ -0,0 +1,11 @@
+/* A dor: um produto tera seu preço alterado, o problema é que devera ter uma aprovação antes.
+vou tilizar database da locadora e os preços serao alterados conforme aprovação.
+desenvolver uma solucao para automatizar esse alteração.
+
+na minha ideia vou fazer uma tabela que devera atualizar "automaticamente", 
+	quando o valor do veiculo for aprovado ele devera atualizar o preço
+a tabela de preco devera atualizar o preço baseado no valor que ele custava e a aprovação sera feita atraves de autorizacao
+	de preferencia com senha e autorizacao no ""ENUM" (sim e nao)
+
+
+*/
